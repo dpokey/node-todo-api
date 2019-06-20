8bb1ef4ef0a6f998b1b157428088f125ffa19941
Delgado Quijandria Pierre Alberto <pdelgadoq@sunat.gob.pe>
2019-06-20 17:39:21 -0500

    Add test GET /todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,12 +4,21 @@ const expect = require('expect')
 const {app} = require('../server')
 const {Todo} = require('../models/todo')
 
+// Añadimos una matriz de objeto que va a ser precargada en la bd para las pruebas
+
+const todos = [{
+    text: 'First test todo'
+}. {
+    text: 'Second test todo'
+}]
+
+
 // beforeEach nos permite correr un codigo antes de cada caso de prueba
 // 1er argumento: done porque las pruebas son asincronas, y solo va a seguir adelante con el caso de pruebas cuando ejecutamos done
 
 beforeEach((done) => {
     // Pasamos un objeto vacio para indicar que queremos borrar todos los docuemntos de la coleccion
-    Todo.deleteMany({}).then(() => done())
+    Todo.deleteMany({}).then(() => return Todo.insertMany(todos).then(done())
 })
 
 
@@ -35,7 +44,7 @@ describe('POST /todos', () => {
                     return done(error)
                 }
                 // validamos si los datos fueron guardados en bd
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     // llamamos a done para concluir el caso de prueba
@@ -56,7 +65,7 @@ describe('POST /todos', () => {
                     return done(error)
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done()
                 }).catch(e => done(e))
             })
