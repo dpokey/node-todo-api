639ed577ea325f5410d2e6bd2fe925f2fe530d6f
Delgado Quijandria Pierre Alberto <pdelgadoq@sunat.gob.pe>
2019-06-27 15:48:24 -0500

    Add tests for PATCH /todos/:id

--- playground/mongodb_connect.js
+++ playground/mongodb_connect.js
@@ -3,23 +3,10 @@ const {MongoClient} = require('mongodb');
 MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }).then((client) => {
     console.log('Connected to MongoDB server')
 
-    const db = client.db('db01')
+    const db = client.db('dbName')
 
-    // drop db01
-    db.dropDatabase().then(result => {
-        console.log('db01 drop')
-        // create db01 again
-        const db = client.db('db01')
+    // Here you can place your operations with the bd
 
-        // insert new collections
-        db.collection('Collections').insertOne({ 
-            text: 'Something to do'
-        }, (error, result) => {
-            if (error) {
-                return console.log('Unable to insert collection', error);            
-            }
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        })
+    client.close();
+}, e => console.log('Error to connect', e))
 
-    },e => console.log(e))
-})
